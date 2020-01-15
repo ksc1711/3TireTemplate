b7ddcffee4f29d6292f0ee1e5d4145db295dade6
ksc1711 <ksc1711@naver.com>
2020-01-15 18:51:55 +0900

    멀티 유저 선택 까지 완료

---
+++ WCMS.Data/PopupData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCMS.Data
+{
+    class PopupData
+    {
+        public int idx { get; set; }
+        public string title { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string joinMember { get; set; }
+        public string descript { get; set; }
+        public string popupHtml { get; set; }
+        public string popupScript { get; set; }
+        public string useYn { get; set; }
+
+    }
+}
