f2464aa71e4c63f0a0c9bfc4c7d15a4db584f7fa
ksc1711 <ksc1711@naver.com>
2020-01-22 09:56:04 +0900

    끝

--- WCMS.Data/PopupData.cs
+++ WCMS.Data/PopupData.cs
@@ -1,32 +1,23 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace WCMS.Data
 {
     public class PopupData
     {
-        [DefaultValue(0)]
         public int idx { get; set; }
-        [DefaultValue("")]
-        public string title{ get; set; }
-        [DefaultValue("")]
+        public string title { get; set; }
         public string startDate { get; set; }
-        [DefaultValue("")]
         public string endDate { get; set; }
-        [DefaultValue("")]
         public string joinMember { get; set; }
-        [DefaultValue("")]
         public string popupType { get; set; }
         public string descript { get; set; }
         public string popupHtml { get; set; }
         public string popupScript { get; set; }
         public string useYn { get; set; }
         public DateTime regDate { get; set; }
-        [DefaultValue("")]
         public string regUser { get; set; }
 
         public DateTime modDate { get; set; }
         public string modUser { get; set; }
-
     }
 }
